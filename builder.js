64ccea6af6ea9b0e9058e88f0cc5ba77988c768b
Antonio Hernandez <ahdiaz@gmail.com>
2016-10-26 23:07:54 +0200

    Improve url helper so it can accept several url parts and concat them

--- builder.js
+++ builder.js
@@ -81,9 +81,23 @@ var debugFiles = function (options) {
     };
 };
 
-Handlebars.registerHelper('url', function(url, options) {
+Handlebars.registerHelper('url', function() {
 
-    var baseUrl = this.globals.site.url[ENV].replace(/\/$/, '');
+    var parts = [];
+    var options = null;
+    var args = Array.prototype.slice.call(arguments);
+
+    while (args.length > 0) {
+        var item = args.shift();
+        if (args.length > 0) {
+            parts.push(item);
+        } else {
+            options = item;
+        }
+    }
+
+    var url = parts.join('');
+    var baseUrl = options.data.root.globals.site.url[ENV].replace(/\/$/, '');
     var parsedUrl = ('/' + Handlebars.escapeExpression(url)).replace(/(\/){2,}/g, '$1');
     parsedUrl = baseUrl + parsedUrl;
 
