49b78ab394634297e7f6d0462f22a1315eb90dab
Antonio Hernandez <ahdiaz@gmail.com>
2016-10-31 21:16:16 +0100

    Only process files with the extension patch

--- patch.js
+++ patch.js
@@ -1,4 +1,5 @@
-var applyPatch = require('apply-patch').applyPatch;
+var applyPatch = require('apply-patch').applyPatch,
+    debug      = require('debug')('ssgs-patch');
 
 // List all files in a directory in Node.js recursively in a synchronous fashion
 var walkSync = function(dir, filelist) {
@@ -14,11 +15,21 @@ var walkSync = function(dir, filelist) {
     if (fs.statSync(path.join(dir, file)).isDirectory()) {
       filelist = walkSync(path.join(dir, file), filelist);
     } else {
-      filelist.push(path.join(dir, file));
+      if (/\.patch$/.test(file)) {
+        filelist.push(path.join(dir, file));
+      }
     }
   });
 
   return filelist;
 };
 
-walkSync('./patches').forEach(applyPatch);
+walkSync('./patches').forEach(function (patch) {
+  try {
+    debug('Applying patch: %s', patch);
+    applyPatch(patch);
+  } catch (e) {
+    debug(e.message);
+    // process.exit(1);
+  }
+});
